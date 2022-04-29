8871b937fa4ba30fab4e4787c39c9e63d1c3cd4d
volk <velvel2k@gmail.com>
2022-04-29 14:20:30 +0300

    added profile modal

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
+
 import ProfileIcon from "./profileIcon/ProfileIcon";
 import classes from "./Navbar.module.css";
 // for now dummy icon
 import profileIcon from "../../testProfile.jpg";
 import logo from "../../logo.png";
 import SearchBar from "./searchBar/SearchBar";
+import ProfilePage from "../pages/profilePage/ProfilePage";
 
 // gets props from app
 const Navbar = (props) => {
-
-    const onLogoClickHandler = () => {
+  const onLogoClickHandler = () => {
     console.log("logo clicked");
   };
   const onMyTaskClickHandler = () => {
--- src/components/navbar/profileIcon/ProfileIcon.jsx
+++ src/components/navbar/profileIcon/ProfileIcon.jsx
@@ -1,17 +1,37 @@
 import classes from "./ProfileIcon.module.css";
+import ProfilePage from "../../pages/profilePage/ProfilePage";
+import logo from "../../../testProfile.jpg";
+import { useState } from "react";
 
 const ProfileIcon = (props) => {
+  const [showProfile, setShowProfile] = useState(false);
+
   const profileClickHandler = () => {
-    console.log("profile clicked");
+    setShowProfile(true);
   };
+
+  const onCloseProfileHandler = () => {
+    setShowProfile(false);
+  };
+
   return (
-    <div className={classes.profile}>
-      <img
-        onClick={profileClickHandler}
-        className={classes.profileImage}
-        src={props.imageSrc}
-        alt="profile"
-      />
+    <div>
+      <div className={classes.profile}>
+        <img
+          onClick={profileClickHandler}
+          className={classes.profileImage}
+          src={props.imageSrc}
+          alt="profile"
+        />
+      </div>
+      {showProfile && (
+        <ProfilePage
+          onCloseProfile={onCloseProfileHandler}
+          imageSrc={logo}
+          username="mike"
+          email="sdcds@"
+        />
+      )}
     </div>
   );
 };
