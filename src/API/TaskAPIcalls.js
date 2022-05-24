e1365596a57bc104df9ad5c3466ac5e63755a7bd
volk <velvel2k@gmail.com>
2022-05-24 12:49:47 +0300

    moved all api calls to a different file

---
+++ src/API/TaskAPIcalls.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const APIaddress = "http://localhost:3002";
+
+export const getAllTasks = async () => {
+  try {
+    const res = axios.get(APIaddress + "/tasks");
+    return (await res).data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const postTask = async (task) => {
+  try {
+    const res = axios.post(APIaddress + "/tasks", task);
+    return await res;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const putEditTask = async (task) => {
+  try {
+    const res = axios.put(`${APIaddress}/tasks/ ${task.task_id}`, task);
+    return await res;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const deleteTask = async (task) => {
+  try {
+    const res = axios.delete(`${APIaddress}/tasks/${task.task_id}`);
+    return await res;
+  } catch (error) {
+    console.log("error", error);
+    return error;
+  }
+};
