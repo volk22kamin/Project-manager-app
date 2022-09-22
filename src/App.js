c685aeaca5a3ad12de96871fe2eddc1bffd929b6
volk <velvel2k@gmail.com>
2022-09-22 23:20:30 +0300

    feat(styling)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { verifyToken, getAllEmails } from "./API/UserAPIcalls";
 import AppContext from "./context/Context";
 
-import { gapi } from "gapi-script";
-
 import LoginPage from "./components/pages/loginPage/LoginPage";
 import Navbar from "./components/navbar/Navbar";
 import ProjectOverview from "./components/pages/projectOverview/ProjectOverview";
@@ -20,18 +18,6 @@ function App() {
   const navigate = useNavigate();
   const context = useContext(AppContext);
   const [isLoggedIn, setIsLoggenIn] = useState(false);
-  const clientId =
-    "567536629255-mqr6h5pkbr3olvugariv2ckpf32tfgif.apps.googleusercontent.com";
-
-  useEffect(() => {
-    const initClient = () => {
-      gapi.client.init({
-        clientId: clientId,
-        scope: "",
-      });
-    };
-    gapi.load("client:auth2", initClient);
-  });
 
   const loginOnToken = async (isNew) => {
     const token = localStorage.getItem("token-promger");
@@ -51,24 +37,11 @@ function App() {
     }
   };
 
-  const loginOnGoogle = () => {
-    setIsLoggenIn(true);
-    navigate("welcome");
-
-    const timer = setTimeout(() => {
-      navigate("allProjects");
-    }, 800);
-    return () => clearTimeout(timer);
-  };
-
   useEffect(() => {
     loginOnToken(false);
   }, []);
 
   const onLogOutHandler = () => {
-    localStorage.removeItem(
-      "oauth2_ss::http://localhost:3000::1::DEFAULT::_ss_"
-    );
     localStorage.removeItem("token-promger");
     setIsLoggenIn(false);
     navigate("/");
@@ -99,7 +72,6 @@ function App() {
             element={
               <LoginPage
                 loginOnToken={loginOnToken}
-                googleLogin={loginOnGoogle}
                 isLoggedIn={isLoggedIn}
                 onLogin={onLogInHandler}
               />
